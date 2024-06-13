77c5a772bfff6ff2581bf7407aaaf11e018e23e6
salvaCasta276 <salCas276@gmail.com>
2024-06-13 11:58:21 -0300

    Implemented basic versions of LSB1 and 4

--- src/main/java/ar/edu/itba/cripto/group4/esteganography/estaganographers/LSB4.java
+++ src/main/java/ar/edu/itba/cripto/group4/esteganography/estaganographers/LSB4.java
@@ -1,17 +1,16 @@
 package ar.edu.itba.cripto.group4.esteganography.estaganographers;
 
 import java.util.stream.Stream;
-import org.jooq.lambda.Seq;
 
 import static org.jooq.lambda.Seq.zip;
 
-public class LSB1 implements Esteganographer{
+public class LSB4 implements Esteganographer{
 
     @Override
     public Stream<Byte> hide(Stream<Byte> image, Stream<Byte> data) {
         Stream<Byte> sparse_data = data.flatMap(b -> Stream.of(unnest_bytes(b)));
         return zip(image, sparse_data).map(x ->
-                (byte)(((x.v1() >> 1) << 1) | x.v2())
+                (byte)(((x.v1() >> 4) << 4) | x.v2())
         ).stream();
     }
 
@@ -21,10 +20,9 @@ public class LSB1 implements Esteganographer{
     }
 
     private Byte[] unnest_bytes(Byte data_byte) {
-        Byte[] ret = new Byte[8];
-        for(int i = 0; i < 8; i++){
-            ret[i] = (byte)((data_byte >> (8 - i - 1)) & 0b00000001);
-        }
+        Byte[] ret = new Byte[2];
+        ret[0] = (byte)((data_byte & 0b11110000) >> 4);
+        ret[1] = (byte)(data_byte & 0b00001111);
         return ret;
     }
 }
