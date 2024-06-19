87ef6b2c32289528ad2d55da2c37f92f52913c33
Nicolas Birsa <33790526+Fegrant@users.noreply.github.com>
2024-06-19 16:09:18 -0300

    fix(LSBI): hide/unhide now working properly (we hope)

--- src/main/java/ar/edu/itba/cripto/group4/steganography/Main.java
+++ src/main/java/ar/edu/itba/cripto/group4/steganography/Main.java
@@ -10,21 +10,28 @@ import ar.edu.itba.cripto.group4.steganography.io.bmp.BmpReaderWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        ArgumentParser argumentParser = new ArgumentParser(args);
+        // ArgumentParser argumentParser = new ArgumentParser(args);
         Steganographer steganographer = new SteganographerImpl();
         final ReaderWriter rw = new BmpReaderWriter();
-        
-        //final var hideInput = Arrays.stream(Utils.stringToBytes("Hola!")).toList().stream();
-        
-        String path = "test_files/ladoLSBI.bmp";
-        final ReaderOutput ro = rw.readFile(Path.of(path));
+
+        Path baseImagePath = Path.of("test_files/ladoLSBI.bmp");
+        ReaderOutput ro = rw.readFile(baseImagePath);
+
+        final var hideInput = Arrays.stream(Utils.stringToBytes("Hola!")).toList().stream();
+        final var hideOutput = steganographer.hide(ro.getData(), hideInput, "hola.txt", SteganographerMethod.LSBI, null);
+
+        Path steggedImagePath = Path.of("steg.bmp");
+        rw.writeFile(steggedImagePath, hideOutput, ro.getMetadata());
+
+        ro = rw.readFile(steggedImagePath);
 
         final var unhideOutput = steganographer.unhide(ro.getData(), ro.getMetadata(), SteganographerMethod.LSBI, null);
 
-        try (FileOutputStream fos = new FileOutputStream("imagen"+ unhideOutput.extension())){
+        try (FileOutputStream fos = new FileOutputStream("salida"+ unhideOutput.extension())){
             unhideOutput.data().forEach(b -> {
                 try {
                     fos.write(b);
@@ -35,8 +42,6 @@ public class Main {
         }
 
         // rw.writeFile(Path.of("image" + unhideOutput.extension()), unhideOutput.data().stream(), ro.getMetadata());
-        
-        // final var hideOutput = steganographer.hide(ro.getData(), hideInput, "hola.txt", SteganographerMethod.LSB1, null);
         
         // rw.writeFile(Path.of("ladoLSB1.bmp"), hideOutput, ro.getMetadata());
         
